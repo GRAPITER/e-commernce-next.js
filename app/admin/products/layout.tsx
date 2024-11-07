4ec51bcdc5777f784e7174ed639f744adc3ecb08
asjad <asjad.ali5968@gmail.com>
2024-11-07 22:38:00 +0500

    add sales order history

--- app/admin/products/layout.tsx
+++ app/admin/products/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SideBar from "./SideBar";
+import SideBar from "../SideBar";
 import { Separator } from "@/components/ui/separator";
 
 export default function DashboardLayout({
